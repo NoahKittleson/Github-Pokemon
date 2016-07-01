a5ac0ff43fd9f2f9d33902052fc8f24a39bd7003
Noah Kittleson <noahkittleson@gmail.com>
2016-07-01 10:57:56 -0700

    hooked up button for submitting words

--- app/src/main/java/com/epicodus/wordgame/GameActivity.java
+++ app/src/main/java/com/epicodus/wordgame/GameActivity.java
@@ -1,16 +1,25 @@
 package com.epicodus.wordgame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.GridView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class GameActivity extends AppCompatActivity {
-    @Bind (R.id.letterGrid) GridView mLetterGrid;
+public class GameActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.letterGrid) GridView mLetterGrid;
+    @Bind(R.id.submitButton) Button mSubmitButton;
+    @Bind(R.id.textView) TextView mTextView;
     private Character[] letters = new Character[] {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'};
+    private ArrayList<String> submittedWords= new ArrayList<String> ();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,15 @@ public class GameActivity extends AppCompatActivity {
 
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, letters);
         mLetterGrid.setAdapter(adapter);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mSubmitButton) {
+            String word = mTextView.getText().toString();
+            submittedWords.add(word);
+        } else if (v == mScoreButton) {
+
+        }
     }
 }
