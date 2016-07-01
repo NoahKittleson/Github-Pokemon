99ac33ea3318b00930be7615f4ecb96990a4e975
Noah Kittleson <noahkittleson@gmail.com>
2016-07-01 11:45:12 -0700

    passing arrayList from gameActivity to scoreActivity

--- app/src/main/java/com/epicodus/wordgame/ScoreActivity.java
+++ app/src/main/java/com/epicodus/wordgame/ScoreActivity.java
@@ -1,13 +1,26 @@
 package com.epicodus.wordgame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class ScoreActivity extends AppCompatActivity {
+    @Bind(R.id.wordListView) ListView mWordList;
+    ArrayList<String> words = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        words = (ArrayList<String>) intent.getSerializableExtra("submittedWords");
     }
 }
