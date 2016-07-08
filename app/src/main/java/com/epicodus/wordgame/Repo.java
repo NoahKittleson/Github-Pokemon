4aa2dc0a4e34b78060c4950e4330f6853f31fffc
Noah Kittleson <noahkittleson@gmail.com>
2016-07-08 14:45:54 -0700

    made model for github repos

---
+++ app/src/main/java/com/epicodus/wordgame/Repo.java
@@ -0,0 +1,29 @@
+package com.epicodus.wordgame;
+
+/**
+ * Created by noahkittleson on 7/8/16.
+ */
+public class Repo {
+    private String mName;
+    private int size;
+    private String languagePrimary;
+    private String languageSecondary;
+
+    public String getLanguageSecondary() {
+        return languageSecondary;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public String getLanguagePrimary() {
+        return languagePrimary;
+    }
+
+
+}
