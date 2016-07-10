154c0b6ff09f3a648d94468d1f9087a29c77521b
Noah Kittleson <noahkittleson@gmail.com>
2016-07-10 15:39:04 -0700

    added RepoDetailFragment.java

--- app/src/main/java/com/epicodus/wordgame/RepoDetailFragment.java
+++ app/src/main/java/com/epicodus/wordgame/RepoDetailFragment.java
@@ -6,24 +6,61 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.parceler.Parcels;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RepoDetailFragment extends Fragment {
+    @Bind(R.id.restaurantImageView) ImageView mImageLabel;
+    @Bind(R.id.restaurantNameTextView) TextView mNameLabel;
+    @Bind(R.id.cuisineTextView) TextView mCategoriesLabel;
+    @Bind(R.id.ratingTextView) TextView mRatingLabel;
+    @Bind(R.id.websiteTextView) TextView mWebsiteLabel;
+    @Bind(R.id.phoneTextView) TextView mPhoneLabel;
+    @Bind(R.id.addressTextView) TextView mAddressLabel;
+    @Bind(R.id.saveRestaurantButton) TextView mSaveRestaurantButton;
 
+    private Repo mRepo;
 
     public RepoDetailFragment() {
-        // Required empty public constructor
+
     }
 
+    public static RepoDetailFragment newInstance(Repo repo) {
+        RepoDetailFragment repoDetailFragment = new RepoDetailFragment();
+        Bundle args = new Bundle();
+        args.putParcelable("repo", Parcels.wrap(repo));
+        repoDetailFragment.setArguments(args);
+        return repoDetailFragment;
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_repo_detail, container, false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mRepo = Parcels.unwrap(getArguments().getParcelable("repo"));
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_repo_detail, container, false);
+        ButterKnife.bind(this, view);
+
+        Picasso.with(view.getContext()).load(mRepo.getImageUrl()).into(mImageLabel);
+
+        mNameLabel.setText(mRepo.getName());
+        mCategoriesLabel.setText(android.text.TextUtils.join(", ", mRepo.getCategories()));
+        mRatingLabel.setText(Double.toString(mRepo.getRating()) + "/5");
+        mPhoneLabel.setText(mRepo.getPhone());
+        mAddressLabel.setText(android.text.TextUtils.join(", ", mRepo.getAddress()));
+
+        return view;
+    }
 }
