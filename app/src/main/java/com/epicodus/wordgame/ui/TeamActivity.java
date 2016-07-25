46348b2aef08cfd5e42cb087f131727bea6357a2
Noah Kittleson <noahkittleson@gmail.com>
2016-07-24 18:17:15 -0700

    added battle button in TeamActivity.java

--- app/src/main/java/com/epicodus/wordgame/ui/TeamActivity.java
+++ app/src/main/java/com/epicodus/wordgame/ui/TeamActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.epicodus.wordgame.Constants;
 import com.epicodus.wordgame.R;
@@ -21,11 +24,12 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 
-public class TeamActivity extends AppCompatActivity {
+public class TeamActivity extends AppCompatActivity implements View.OnClickListener {
     private DatabaseReference mRepoReference;
     private FirebaseRecyclerAdapter mFirebaseAdapter;
 
     @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+    @Bind(R.id.button) Button mBattleButton;
 
 
     @Override
@@ -63,5 +67,10 @@ public class TeamActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         mFirebaseAdapter.cleanup();
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(this, "Battle system not made yet.", Toast.LENGTH_SHORT).show();
     }
 }
